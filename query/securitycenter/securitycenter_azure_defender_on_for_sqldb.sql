1db71760eb912c1cb94615ba0d2dbccafed640f3
rajmohanty17 <raj@turbot.com>
2022-01-13 19:01:30 +0530

    Add securitycenter queries

---
+++ query/securitycenter/securitycenter_azure_defender_on_for_sqldb.sql
@@ -0,0 +1,15 @@
+select
+  type || ' ' || name as resource,
+  case
+    when (arguments ->> 'resource_type') = 'SqlServers' and (arguments ->> 'tier') = 'Standard' then 'ok'
+    else 'info'
+  end status,
+  name || case
+    when (arguments ->> 'resource_type') = 'SqlServers' and (arguments ->> 'tier') = 'Standard' then ' Azure Defender on for SQL database servers'
+    else ' Azure Defender off for SQL database servers'
+  end || '.' reason,
+  path
+from
+  terraform_resource
+where
+  type = 'azurerm_security_center_subscription_pricing';
---
+++ query/securitycenter/securitycenter_email_configured.sql
@@ -0,0 +1,15 @@
+select
+  type || ' ' || name as resource,
+  case
+    when (arguments -> 'email') is not null and (arguments -> 'alert_notifications')::bool is true then 'ok'
+    else 'alarm'
+  end status,
+  name || case
+    when (arguments -> 'email') is not null and (arguments -> 'alert_notifications')::bool is true then ' additional email & alert notifications configured'
+    else ' additional email & alert notifications not configured'
+  end || '.' reason,
+  path
+from
+  terraform_resource
+where
+  type = 'azurerm_security_center_contact';
