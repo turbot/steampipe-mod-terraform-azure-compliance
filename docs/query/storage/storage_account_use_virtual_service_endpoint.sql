b1bdfff61b2a75fa803ff1a14233f884fab7dcad
misraved <ved@turbot.com>
2022-01-24 17:22:45 +0530

    Add remaining queries for azure storage accounts

---
+++ docs/query/storage/storage_account_use_virtual_service_endpoint.sql
@@ -0,0 +1,34 @@
+with storage_account_network_rules as (
+    select 
+      name,
+      type,
+      path,
+      split_part((arguments ->> 'storage_account_name'), '.',2) as storage_account_name
+    from 
+      terraform_resource
+    where
+      type = 'azurerm_storage_account_network_rules' and (arguments ->> 'default_action') = 'Deny'
+), storage_account_name as (
+    select
+      name,
+      type,
+      path
+    from 
+      terraform_resource
+    where
+      type = 'azurerm_storage_account'
+)
+select
+  san.type || ' ' || san.name as resource,
+  case
+    when sanr.name is null then 'alarm'
+    else 'ok'
+  end status,
+  san.name || case
+    when sanr.name is null then ' does not use virtual service endpoint'
+    else ' uses virtual service endpoint'
+  end || '.' reason,
+  san.path
+from
+  storage_account_name as san
+  left join storage_account_network_rules as sanr on sanr.storage_account_name = san.name;
