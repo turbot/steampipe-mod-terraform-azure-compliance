729d69555076ecb81c02e35cb18cead7ecb9ef6b
misraved <ved@turbot.com>
2022-01-25 12:57:32 +0530

    Add queries for sql, frontdoor, healthcare and iot

---
+++ query/healthcare/healthcare_fhir_azure_api_encrypted_at_rest_with_cmk.sql
@@ -0,0 +1,15 @@
+select
+  type || ' ' || name as resource,
+  case
+    when (arguments -> 'cosmosdb_key_vault_key_versionless_id') is null then 'alarm'
+    else 'ok'
+  end status,
+  name || case
+    when (arguments -> 'cosmosdb_key_vault_key_versionless_id') is null then ' not encrypted with CMK'
+    else ' encrypted with CMK'
+  end || '.' reason,
+  path
+from
+  terraform_resource
+where
+  type = 'azurerm_healthcare_service';
