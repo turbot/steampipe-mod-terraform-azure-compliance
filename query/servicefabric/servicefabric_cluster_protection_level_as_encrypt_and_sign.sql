df47868fcedb57b194c863bb68cfa4f5d7f77913
misraved <ved@turbot.com>
2022-01-25 15:28:15 +0530

    Add queries for service fabric cluster

---
+++ query/servicefabric/servicefabric_cluster_protection_level_as_encrypt_and_sign.sql
@@ -0,0 +1,19 @@
+select
+  type || ' ' || name as resource,
+  case
+    when (arguments -> 'fabric_settings') is null then 'alarm'
+    when (arguments -> 'fabric_settings' -> 'parameters') is null then 'alarm'
+    when (arguments -> 'fabric_settings' ->> 'parameters') like '%EncryptAndSign%' then 'ok'
+    else 'alarm'
+  end status,
+  name || case
+    when (arguments -> 'fabric_settings') is null then ' Cluster Protection Level not set'
+    when (arguments -> 'fabric_settings' -> 'parameters') is null then ' Cluster Protection Level not set to EncryptAndSign'
+    when (arguments -> 'fabric_settings' ->> 'parameters') like '%EncryptAndSign%' then ' Cluster Protection Level set to EncryptAndSign'
+    else ' Cluster Protection Level not set to EncryptAndSign'
+  end || '.' reason,
+  path
+from
+  terraform_resource
+where
+  type = 'azurerm_service_fabric_cluster';
