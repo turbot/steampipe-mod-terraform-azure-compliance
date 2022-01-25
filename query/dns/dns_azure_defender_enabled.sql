b953b0508a55c704d9ec50e6b19b05fda03fbfe7
rajmohanty17 <raj@turbot.com>
2022-01-25 16:03:35 +0530

    Added dns query

---
+++ query/dns/dns_azure_defender_enabled.sql
@@ -0,0 +1,15 @@
+select
+  type || ' ' || name as resource,
+  case
+    when (arguments ->> 'resource_type') = 'Dns' and (arguments ->> 'tier') = 'Standard' then 'ok'
+    else 'skip'
+  end status,
+  name || case
+    when (arguments ->> 'resource_type') = 'Dns' and (arguments ->> 'tier') = 'Standard' then ' Dns azure defender enabled'
+    else ' Dns azure defender disabled'
+  end || '.' reason,
+  path
+from
+  terraform_resource
+where
+  type = 'azurerm_security_center_subscription_pricing';
