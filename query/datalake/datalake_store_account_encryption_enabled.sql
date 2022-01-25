a9fa89f036d8490f09b6f6e34b5a035e347d3170
Raj <raj@turbot.com>
2022-01-25 11:25:44 +0530

    Add security center queries

---
+++ query/datalake/datalake_store_account_encryption_enabled.sql
@@ -0,0 +1,15 @@
+select
+  type || ' ' || name as resource,
+  case
+    when (arguments ->> 'encryption_state') = 'Enabled' then 'ok'
+    else 'alarm'
+  end status,
+  name || case
+    when (arguments ->> 'encryption_state') = 'Enabled' then ' encryption at rest enabled'
+    else ' encryption at rest disabled'
+  end || '.' reason,
+  path
+from
+  terraform_resource
+where
+  type = 'azurerm_data_lake_store';
